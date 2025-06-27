e527f6084f4a3f592d06c25e34e08cc3769706a8
Paul Buetow <paul@buetow.org>
2025-06-27 23:13:16 +0300

    fix unit test

--- internal/task/stats_test.go
+++ internal/task/stats_test.go
@@ -10,8 +10,8 @@ func TestStats(t *testing.T) {
 	tasks := []Task{
 		{Description: "t1"},
 		{Description: "t2", Start: "20240101T000000Z"},
-		{Description: "t3", Due: now.Add(-time.Hour).Format("20060102T150405Z")},
-		{Description: "t4", Start: "20240101T000000Z", Due: now.Add(-time.Hour).Format("20060102T150405Z")},
+		{Description: "t3", Due: now.Add(-time.Hour).UTC().Format("20060102T150405Z")},
+		{Description: "t4", Start: "20240101T000000Z", Due: now.Add(-time.Hour).UTC().Format("20060102T150405Z")},
 	}
 
 	if TotalTasks(tasks) != 4 {
